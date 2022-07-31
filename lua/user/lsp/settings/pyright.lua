4185e4e49a414e7d56cef5a768c542198440f44d
Jason Kary <jkary@cisco.com>
2022-07-31 06:45:55 -0400

    Setup python path for pyright to use .venv

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,3 +1,25 @@
+local configs = require("lspconfig/configs")
+local util = require("lspconfig/util")
+local path = util.path
+
+local function setup_venv_dir(workspace) 
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+
+  -- Find and use virtualenv in workspace directory.
+  for _, pattern in ipairs({'*', '.*'}) do
+    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+    if match ~= '' then
+      return path.join(path.dirname(match), 'bin', 'python')
+    end
+  end
+
+  -- Fallback to system Python.
+  return exepath('python3') or exepath('python') or 'python'
+end
+
 return {
 	settings = {
 		python = {
@@ -6,4 +28,7 @@ return {
 			},
 		},
 	},
+    before_init = function(_, config)
+        config.settings.python.pythonPath = setup_venv_dir(config.root_dir)
+    end
 }
